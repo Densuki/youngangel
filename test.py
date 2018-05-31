b17b371f226c59ee323aab28de41120c954094ea
Densuki <15634616+Densuki@users.noreply.github.com>
2018-05-30 22:06:14 -0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,59 @@
+# ===================================================
+# IMPORT'S
+# ===================================================
+
+import discord
+import asyncio
+
+import secreto
+
+# ----------------------------------------------------
+
+from discord.ext import commands
+
+# ----------------------------------------------------
+
+# ===================================================
+client = discord.Client()
+# ===================================================
+COR = 0x690FC3
+Token = secreto.seu_token()
+msg_id = None
+msg_user = None
+
+# ===================================================
+# IDENTIDADE PELO CONSOLE
+# ===================================================
+
+@client.event
+async def on_ready():
+    print('===================================================')
+    print('BOT ONLINE: - Olá Mundo!')
+    print(client.user.name)
+    print(client.user.id)
+    print('===================================================')
+    print('Version - 1.2')
+    print('===================================================')
+
+
+# ===================================================
+# TEST
+# ===================================================
+
+# ===================================================
+# LOOP BREAK
+# ===================================================
+
+@client.event
+async def on_message(text):
+    if text.author.id == '451510020614389761': return # Berserker Debug
+
+# ===================================================
+# MODELO PARA COMANDOS
+# ===================================================
+
+@client.event
+async def on_message(message):  # Condição
+    if message.content.lower().startswith('test'):  # PREFIX DO COMANDO (Deste, no caso)
+        await client.send_message(message.channel,
+                                  "**Estar tudo certo!**")  # Mensagem como resultado

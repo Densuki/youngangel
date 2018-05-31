db0cb112dd7db2d225881e8ca913cff7ac6d6716
Densuki <15634616+Densuki@users.noreply.github.com>
2018-05-30 21:25:30 -0300

    Update youngangel.py

--- youngangel.py
+++ youngangel.py
@@ -62,7 +62,7 @@ async def on_ready():
 async def on_message(message): #Condição
 
     if message.content.lower().startswith('#cargos'):  #BASE PARA CARGOS
-     if message.author.id == "336311215099740160":  # permissão por ID
+     #if message.author.id == "336311215099740160":  # permissão por ID
       embed1 = discord.Embed(
         title="**Cargos Superiores**\n "
               ,

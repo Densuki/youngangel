66df15f263da081506fa02d629ef99345acf71d2
Densuki <15634616+Densuki@users.noreply.github.com>
2018-06-02 00:07:47 -0300

    Update cargos.py

--- cargos.py
+++ cargos.py
@@ -9,7 +9,8 @@ def cargos_roles();
 async def on_message(message): #Condição
 
     if message.content.lower().startswith('#cargos'):  #BASE PARA CARGOS
-     if message.author.id == "286206456108154880":  # permissão por ID [EU]
+    # if message.author.id == "286206456108154880":  # permissão por ID [DINO]
+     if message.author.id == "336311215099740160":  # permissão por ID [EU]
       embed1 = discord.Embed(
         title="**Cargos Superiores**\n "
               ,

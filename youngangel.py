dac8ed5805d950bec6af3cd6068d133072c8088b
Densuki <15634616+Densuki@users.noreply.github.com>
2018-06-02 00:09:13 -0300

    Update youngangel.py

--- youngangel.py
+++ youngangel.py
@@ -11,6 +11,7 @@ client = discord.Client()
 #===================================================
 COR = 0x690FC3
 Token = secreto.seu_token()
+Roles = cargos.cargos_roles()
 
 msg_id = None
 msg_user = None
@@ -55,119 +56,7 @@ async def on_message(message):  # Condição
 # ===================================================
 #CARGOS
 # ===================================================
-@client.event
-async def on_message(message): #Condição
 
-    if message.content.lower().startswith('#cargos'):  #BASE PARA CARGOS
-     if message.author.id == "286206456108154880":  # permissão por ID [EU]
-      embed1 = discord.Embed(
-        title="**Cargos Superiores**\n "
-              ,
-        color=COR,
-        description="Escolha o cargo que será proposto!\n"
-                    "- Fundador = ⚡🌌⚡\n"
-                    "- Dono(a)  =  ⭐⭐ \n"
-                    "- adm  = ⭐ \n" 
-                    "- Bomber Rank S  = ⚜🔰 \n"
-                    "- Bomber Rank A  = 🔧 \n"
-                    "- Bomber Rank B  = 🛠 \n"
-                    "- Bomber Rank C  = 🔨",)
-
-     botmsg = await client.send_message(message.channel, embed=embed1)
-
-     await client.add_reaction(botmsg, "🌌")
-     await client.add_reaction(botmsg, "⚡")
-     await client.add_reaction(botmsg, "⭐")
-     await client.add_reaction(botmsg, "⚜")
-     await client.add_reaction(botmsg, "🔧")
-     await client.add_reaction(botmsg, "🛠")
-     await client.add_reaction(botmsg, "🔨")
-
-
-     global msg_id
-     msg_id = botmsg.id
-
-     global msg_user
-     msg_user = message.author
-
-
-@client.event
-async def on_reaction_add(reaction, user):
-    msg = reaction.message
-
-    if reaction.emoji == "🌌" and msg.id == msg_id: #and user == msg_user:
-     role = discord.utils.find(lambda r: r.name == "🌀⚡Fundador🌀⚡", msg.server.roles)
-     await client.add_roles(user, role)
-     print("add")
-
-    if reaction.emoji == "⚡" and msg.id == msg_id: #and user == msg_user:
-     role = discord.utils.find(lambda r: r.name == "⭐⭐Dono(a)⭐⭐", msg.server.roles)
-     await client.add_roles(user, role)
-     print("add")
-
-    if reaction.emoji == "⭐" and msg.id == msg_id: #and user == msg_user:
-     role = discord.utils.find(lambda r: r.name == "adm", msg.server.roles)
-     await client.add_roles(user, role)
-     print("add")
-
-    if reaction.emoji == "⚜" and msg.id == msg_id: #and user == msg_user:
-     role = discord.utils.find(lambda r: r.name == "Bomber Rank S", msg.server.roles)
-     await client.add_roles(user, role)
-     print("add")
-
-    if reaction.emoji == "🔧" and msg.id == msg_id: #and user == msg_user:
-     role = discord.utils.find(lambda r: r.name == "Bomber Rank A", msg.server.roles)
-     await client.add_roles(user, role)
-     print("add")
-
-    if reaction.emoji == "🛠" and msg.id == msg_id: #and user == msg_user:
-     role = discord.utils.find(lambda r: r.name == "Bomber Rank B", msg.server.roles)
-     await client.add_roles(user, role)
-     print("add")
-
-    if reaction.emoji == "🔨" and msg.id == msg_id: #and user == msg_user:
-     role = discord.utils.find(lambda r: r.name == "Bomber Rank C", msg.server.roles)
-     await client.add_roles(user, role)
-     print("add")
-
-@client.event
-async def on_reaction_remove(reaction, user):
-    msg = reaction.message
-
-    if reaction.emoji == "🌌" and msg.id == msg_id: #and user == msg_user:
-     role = discord.utils.find(lambda r: r.name == "🌀⚡Fundador🌀⚡", msg.server.roles)
-     await client.remove_roles(user, role)
-     print("remove")
-
-    if reaction.emoji == "⚡" and msg.id == msg_id: #and user == msg_user:
-     role = discord.utils.find(lambda r: r.name == "⭐⭐Dono(a)⭐⭐", msg.server.roles)
-     await client.remove_roles(user, role)
-     print("remove")
-
-    if reaction.emoji == "⭐" and msg.id == msg_id: #and user == msg_user:
-     role = discord.utils.find(lambda r: r.name == "adm", msg.server.roles)
-     await client.remove_roles(user, role)
-     print("remove")
-
-    if reaction.emoji == "⚜" and msg.id == msg_id: #and user == msg_user:
-     role = discord.utils.find(lambda r: r.name == "Bomber Rank S", msg.server.roles)
-     await client.remove_roles(user, role)
-     print("remove")
-
-    if reaction.emoji == "🔧" and msg.id == msg_id: #and user == msg_user:
-     role = discord.utils.find(lambda r: r.name == "Bomber Rank A", msg.server.roles)
-     await client.remove_roles(user, role)
-     print("remove")
-
-    if reaction.emoji == "🛠" and msg.id == msg_id: #and user == msg_user:
-     role = discord.utils.find(lambda r: r.name == "Bomber Rank B", msg.server.roles)
-     await client.remove_roles(user, role)
-     print("remove")
-
-    if reaction.emoji == "🔨" and msg.id == msg_id: #and user == msg_user:
-     role = discord.utils.find(lambda r: r.name == "Bomber Rank C", msg.server.roles)
-     await client.remove_roles(user, role)
-     print("remove")
 
 #===================================================
 #MÚSICA
